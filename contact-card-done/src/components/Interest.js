dc4605ec4ca955f9997e0aa6ceb9485c8d7c3c75
Karthik Goud R V <karthikrv09@gmail.com>
2022-06-03 18:59:10 +0530

    scrimba solo project react contact card

---
+++ contact-card-done/src/components/Interest.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Interest() {
+  return (
+    <div className="interest-section">
+      <h2>Interest</h2>
+      <p>
+        n publishing and graphic design, Lorem ipsum is a placeholder text
+        commonl relying on meaningful content. Lorem ipsum may be
+      </p>
+    </div>
+  );
+}
