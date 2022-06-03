8e6c855b8f9bb8c7fa20f19bd0e73b7f8659ded6
Karthik Goud R V <karthikrv09@gmail.com>
2022-06-03 19:11:00 +0530

    final contact code with css

--- React-Scrimba/index.js
+++ React-Scrimba/index.js
@@ -1,14 +1,6 @@
-// import React from "react";
-// import ReactDOM from "react-dom";
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import "./index.css";
 
-const Hello = (
-  <div>
-    <h1>Hello React...</h1>
-    <ul>
-      <li> </li>
-      <li>react is composable</li>
-    </ul>
-  </div>
-);
-
-ReactDOM.render(Hello, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
