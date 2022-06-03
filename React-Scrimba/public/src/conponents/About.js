6f4e1c634233cad5729514f9ccbbc629a9723a11
karthikgoud <karthikrv09@gmail.com>
2022-06-03 19:22:52 +0530

    reorder 2

---
+++ React-Scrimba/public/src/conponents/About.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <div className="about-section">
+      <h2>About</h2>
+      <p>
+        I am the publishing and graphic design, Lorem ipsum is a placeholder
+        text commonly used to demonstrate the visual form of a document or a
+        typeface without relying on meaningful content. Lorem ipsum may be
+      </p>
+    </div>
+  );
+}
