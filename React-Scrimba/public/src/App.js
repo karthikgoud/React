18c764226d761edcb896fd2b53dd9db4b6cefaad
karthikgoud <karthikrv09@gmail.com>
2022-06-05 19:17:23 +0530

    asdasd

--- React-Scrimba/public/src/App.js
+++ React-Scrimba/public/src/App.js
@@ -3,14 +3,3 @@ import Info from "./components/Info";
 import About from "./components/About";
 import Interest from "./components/Interest";
 import Footer from "./components/Footer";
-
-export default function App() {
-  return (
-    <div className="container">
-      <Info />
-      <About />
-      <Interest />
-      <Footer />
-    </div>
-  );
-}
